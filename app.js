3b048313b291f80441f8f1500186007495c5eeb5
Batsaikhan <batsaikhan0408@gmail.com>
2022-01-29 19:57:40 +0800

    toglogchiin eeljiig soliloo

--- app.js
+++ app.js
@@ -20,8 +20,31 @@ window.document.getElementById("current-0").textContent = "0";
 window.document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+// shoog shideh eventlistener
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 sanamsargui too gargah
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  // shoonii zurgiig web deer gargah
   diceDom.style.display = "block";
+  // buusan toonii zurgiig gargah
   diceDom.src = "dice-" + diceNumber + ".png";
+  //buusan tooon 1ees ylgaaatai bol idevhtei toglogchiin eeljiin onoog nemegduulne
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    //1 buulgasan toglogchiin onoog 0 bolgono
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    // activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    if (activePlayer === 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    }
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    // shoog tur alga bolgono.
+    diceDom.style.display = "none";
+  }
 });
